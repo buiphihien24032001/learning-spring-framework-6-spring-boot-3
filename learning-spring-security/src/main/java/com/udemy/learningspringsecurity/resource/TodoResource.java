5f5c819a3f3338f8dfbca9b579c82790b0c7a2e9
Hien Bui Phi <hien.bui@bstarsolutions.com>
2023-08-24 13:27:28 +0700

    DAY-24-8-2023: section 16 - done step 22

--- learning-spring-security/src/main/java/com/udemy/learningspringsecurity/resource/TodoResource.java
+++ learning-spring-security/src/main/java/com/udemy/learningspringsecurity/resource/TodoResource.java
@@ -1,7 +1,11 @@
 package com.udemy.learningspringsecurity.resource;
 
+import jakarta.annotation.security.RolesAllowed;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,8 +18,8 @@ public class TodoResource {
     private Logger logger = LoggerFactory.getLogger(getClass());
 
     private static final List<Todo> TODOS_LIST =
-            List.of(new Todo("in28minutes", "Learn AWS"),
-                    new Todo("in28minutes", "Get AWS Certified"));
+            List.of(new Todo("James", "Learn AWS"),
+                    new Todo("James", "Get AWS Certified"));
 
     @GetMapping("/todos")
     public List<Todo> retrieveAllTodos() {
@@ -23,6 +27,10 @@ public class TodoResource {
     }
 
     @GetMapping("/users/{username}/todos")
+    @PreAuthorize("hasRole('USER') and #username == authentication.name")
+    @PostAuthorize("returnObject.username == 'James'")
+    @RolesAllowed({"ADMIN", "USER"})
+    @Secured({"ROLE_ADMIN", "ROLE_USER"})
     public Todo retrieveTodosForSpecificUser(@PathVariable String username) {
         return TODOS_LIST.get(0);
     }
