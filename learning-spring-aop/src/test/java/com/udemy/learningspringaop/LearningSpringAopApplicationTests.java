0a87f1dc294348820f0503461a8b3a4b38b160ce
Hien Bui Phi <hien.bui@bstarsolutions.com>
2023-08-25 09:06:14 +0700

    DAY-24-8-2023: section 17 - step 5

---
+++ learning-spring-aop/src/test/java/com/udemy/learningspringaop/LearningSpringAopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.learningspringaop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringAopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
